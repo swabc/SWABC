6b15671d20f346570f021d79a6f613fded43730c
swabc <50553884+swabc@users.noreply.github.com>
2019-05-18 16:56:38 +0800

    Create cswtypes.h

---
+++ Software/mc/src/csw/common/inc/cswtypes.h
@@ -0,0 +1,12 @@
+#ifndef  __CSW_TYPES_H
+#define  __CSW_TYPES_H
+
+typedef unsigned char  bool;
+typedef unsigned char  uint8;
+typedef unsigned short uint16;
+typedef unsigned long  uint32;
+typedef signed   char  sint8;
+typedef signed   short sint16;
+typedef signed   long  sint32;
+
+#endif // __CSW_TYPES_H
