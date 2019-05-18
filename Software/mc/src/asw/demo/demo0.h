52a179ab70a861d990c4f4d665a8d7f6353a59a7
swabc <50553884+swabc@users.noreply.github.com>
2019-05-18 11:15:17 +0800

    Create demo0.h

---
+++ Software/mc/src/asw/demo/demo0.h
@@ -0,0 +1,25 @@
+#ifndef __DEMO_0_H
+#define __DEMO_0_H
+
+/* initialization of this module, 
+ * called by system init after POR */
+extern void demo0_init(void);
+
+/* task of this module,
+ * called by system scheduler cyclically,
+ * handle the most of the functional logics */
+extern void demo0_task(void);
+
+/* required input interface,
+ * this module gets the button state by calling this interface.
+ * return value: TRUE - the button is pressed
+ *               FALSE - the button is not pressed or unknown */
+extern bool demo0_ri_getButtonState(void);
+
+/* required output interface,
+ * this module turns the LED on/off by calling this interface.
+ * argument state: TRUE - the LED will turns on,
+ *                 FALSE - the LED will turns off. */
+extern void demo0_ro_setLedState(bool state);
+
+#endif // __DEMO_0_H
