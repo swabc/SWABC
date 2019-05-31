3c9715c700dfa2a8a36096023d91e93f7e8ba804
swabc <50553884+swabc@users.noreply.github.com>
2019-05-31 10:58:35 +0800

    Create timer.h

---
+++ Software/mc/src/csw/module/timer/timer.h
@@ -0,0 +1,22 @@
+#ifndef  __TIMER_H
+#define  __TIMER_H
+
+typedef void (*timeOutCallback_t)(void);
+
+typedef struct
+{
+    uint8   active:1;
+    uint16  counter;
+    uint16  cycle;
+    timeOutCallback_t  timeOutCallback;
+}
+timer_t;
+
+extern void timer_init(timer_t * t, uint16 cycle, timeOutCallback_t tocb);
+extern void timer_task(timer_t * t);
+
+extern void timer_start(timer_t * t);
+extern void timer_stop(timer_t * t);
+extern uint8 timer_isTimeOut(timer_t * t);
+
+#endif // __TIMER_H
